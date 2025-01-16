cba9e3c6816bfed8cf2fd4e513e819320ad9e26f
Francisco Roman <franciscoromansierra@gmail.com>
2025-01-15 23:50:03 -0500

    adding fonts

--- components/NewRecipes/NewRecipes.tsx
+++ components/NewRecipes/NewRecipes.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import style from "./NewRecipes.module.scss";
+import { monsterClubhouse } from "@/lib/fonts";
 
 export const NewRecipes = () => {
   return (
     <section className={style.newRecipes}>
       <div className={style.title}>
-        <h3>Nuevas Recetas</h3>
+        <h3 className={`${monsterClubhouse.className}`}>Nuevas Recetas</h3>
       </div>
       <div>{/** data del api */}</div>
     </section>
--- components/banner/banner.tsx
+++ components/banner/banner.tsx
@@ -1,10 +1,11 @@
+import { poppins } from "@/lib/fonts";
 import style from "./banner.module.scss";
 
 export const Banner = () => {
   return (
     <section className={style.banner}>
       <div>
-        <h2>Recetas</h2>
+        <h2 className={`${poppins.className}`}>Recetas</h2>
         <span>para todos</span>
       </div>
     </section>
