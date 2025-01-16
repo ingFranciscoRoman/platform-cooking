ba527ccf6f1531e62c7c3a287cb25ca5a5cef810
Francisco Roman <franciscoromansierra@gmail.com>
2025-01-16 14:03:06 -0500

    WIP

---
+++ components/atoms/Heading/index.tsx
@@ -0,0 +1,18 @@
+import styles from "./Heading.module.scss";
+
+interface HeadingProps {
+  levels?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+  children: React.ReactNode;
+  style?: React.CSSProperties;
+}
+
+const Heading: React.FC<HeadingProps> = ({ levels, children, style }) => {
+  const Tag = levels || "h1";
+  return (
+    <Tag className={styles.heading} style={style}>
+      {children}
+    </Tag>
+  );
+};
+
+export default Heading;
--- components/organisms/Footer/index.tsx
+++ components/organisms/Footer/index.tsx
@@ -1,11 +1,19 @@
 import { monsterClubhouse } from "@/lib/fonts";
 import style from "./Footer.module.scss";
+import Image from "next/image";
 
 export const Footer = () => {
   return (
     <footer className={style.containerFooter}>
       <h1 className={`${monsterClubhouse.className}`}>Con el Patrocinio de</h1>
-      <div>imagenes</div>
+      <div>
+        <Image
+          src="/assets/products.png"
+          alt="productus"
+          width={100}
+          height={100}
+        />
+      </div>
     </footer>
   );
 };
