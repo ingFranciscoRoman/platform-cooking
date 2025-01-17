306a102f8bb138f84cbb8aa67007c9a5fa04fc92
Francisco Roman <franciscoromansierra@gmail.com>
2025-01-16 22:00:32 -0500

    WIP

--- components/organisms/Footer/index.tsx
+++ components/organisms/Footer/index.tsx
@@ -1,19 +1,11 @@
-import { monsterClubhouse } from "@/lib/fonts";
 import style from "./Footer.module.scss";
 import Image from "next/image";
 
 export const Footer = () => {
   return (
     <footer className={style.containerFooter}>
-      <h1 className={`${monsterClubhouse.className}`}>Con el Patrocinio de</h1>
-      <div>
-        <Image
-          src="/assets/products.png"
-          alt="productus"
-          width={100}
-          height={100}
-        />
-      </div>
+      <h1 className="primari-m-title">Con el Patrocinio de</h1>
+      <Image src="/img/products.png" alt="products" width={536} height={248} />
     </footer>
   );
 };
