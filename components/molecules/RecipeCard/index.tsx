09adcb5f4391851620bbe2f68f8a0f2eddf202ff
Francisco Roman <franciscoromansierra@gmail.com>
2025-01-19 18:09:57 -0500

    WIP

--- components/molecules/RecipeCard/index.tsx
+++ components/molecules/RecipeCard/index.tsx
@@ -29,53 +29,54 @@ function RecipeCard({
   iconTime,
 }: RecipeCardProps) {
   const { firstTitle, secondTitle } = useSplitTitle(title);
-
+  //TODO: problemas con el responsive desing
   return (
-    <section className={style.cardContainer}>
-      <div className={style["container-img"]}>
-        <Image src={image} alt={title} width={160} height={157} />
-      </div>
-      <div className={style.card}>
-        <div className={style.cardContent}>
-          <div className={style["content-title"]}>
-            <p className="font-card-title">{firstTitle}</p>
-            <p className="font-card-title-recipe">{secondTitle}</p>
-          </div>
-          <div className={style["content-icons"]}>
-            <div className={style["content-icons-start"]}>
-              <Image
-                src="/img/ic_star.svg"
-                alt="icon-portion"
-                width={20}
-                height={20}
-              />
-              <p className="font-card-title">{aggregateLikes}</p>
-            </div>
-            <div className={style["content-icons-heart"]}>
-              <Image
-                src="/img/ic_heart.svg"
-                alt="icon-portion"
-                width={20}
-                height={20}
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-      {/* <Image src={image} alt={title} width={160} height={157} /> */}
-      <div className={style.hiddenCard}>
-        <BackInformationCard
-          image={`${image}`}
-          title={`${title}`}
-          servings={servings}
-          readyInMinutes={readyInMinutes}
-          difficulty={difficulty}
-          iconPortion={iconPortion}
-          iconTime={iconTime}
-          iconDiff={iconDiff}
-        />
-      </div>
-    </section>
+    <>Contenedor de card</>
+    // <section className={style.cardContainer}>
+    //   <div className={style["container-img"]}>
+    //     <Image src={image} alt={title} width={160} height={157} />
+    //   </div>
+    //   <div className={style.card}>
+    //     <div className={style.cardContent}>
+    //       <div className={style["content-title"]}>
+    //         <p className="font-card-title">{firstTitle}</p>
+    //         <p className="font-card-title-recipe">{secondTitle}</p>
+    //       </div>
+    //       <div className={style["content-icons"]}>
+    //         <div className={style["content-icons-start"]}>
+    //           <Image
+    //             src="/img/ic_star.svg"
+    //             alt="icon-portion"
+    //             width={20}
+    //             height={20}
+    //           />
+    //           <p className="font-card-title">{aggregateLikes}</p>
+    //         </div>
+    //         <div className={style["content-icons-heart"]}>
+    //           <Image
+    //             src="/img/ic_heart.svg"
+    //             alt="icon-portion"
+    //             width={20}
+    //             height={20}
+    //           />
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    //   {/* <Image src={image} alt={title} width={160} height={157} /> */}
+    //   <div className={style.hiddenCard}>
+    //     <BackInformationCard
+    //       image={`${image}`}
+    //       title={`${title}`}
+    //       servings={servings}
+    //       readyInMinutes={readyInMinutes}
+    //       difficulty={difficulty}
+    //       iconPortion={iconPortion}
+    //       iconTime={iconTime}
+    //       iconDiff={iconDiff}
+    //     />
+    //   </div>
+    // </section>
   );
 }
 
