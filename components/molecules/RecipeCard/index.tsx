5d6180750a2180b2a5aee1765ab5bb970a6dc4b8
Francisco Roman <franciscoromansierra@gmail.com>
2025-01-20 00:24:03 -0500

    change name of file, sass in RecipeCard

--- components/molecules/RecipeCard/index.tsx
+++ components/molecules/RecipeCard/index.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 // import style from "@/components/molecules/RecipeCard/RecipeCard.module.scss";
-import style from "@/components/molecules/RecipeCard/test.module.scss";
+import style from "@/components/molecules/RecipeCard/RecipeCard.module.scss";
 import { useSplitTitle } from "@/components/molecules/RecipeCard/hooks/useSplitTitle";
 import BackInformationCard from "@/components/atoms/BackInformationCard";
 
