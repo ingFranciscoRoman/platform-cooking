a9d35d843e59d5e0d9f502ac9d58e00b1000ca58
Francisco Roman <franciscoromansierra@gmail.com>
2025-01-20 11:51:39 -0500

    Adding styles to nav item fonts

--- components/organisms/SectionRecipes/index.tsx
+++ components/organisms/SectionRecipes/index.tsx
@@ -9,8 +9,59 @@ const getDifficulty = (readyInMinutes: number) => {
   return "difícil";
 };
 
+export const recipes = [
+  {
+    id: 1,
+    title: "Korean Sweet n Sour Chicken",
+    servings: 4,
+    readyInMinutes: 30,
+    difficulty: getDifficulty(30),
+    aggregateLikes: 2,
+    image: "/img/ic_ojingeo_muchim.png", // https://img.spoonacular.com/recipes/638369-556x370.jpg
+    iconDiff: "/img/diff.svg",
+    iconPortion: "/img/porcion.svg",
+    iconTime: "/img/timepre.svg",
+  },
+  {
+    id: 2,
+    title: "Royal Wedding Pear and Coriander Scones",
+    servings: 8,
+    readyInMinutes: 45,
+    difficulty: getDifficulty(45),
+    aggregateLikes: 3,
+    image: "/img/ic_cola_chicken.png", // https://img.spoonacular.com/recipes/658850-556x370.jpg
+    iconDiff: "/img/diff.svg",
+    iconPortion: "/img/porcion.svg",
+    iconTime: "/img/timepre.svg",
+  },
+  {
+    id: 3,
+    title: "White Chocolate Cranberry Ice Cream",
+    servings: 6,
+    readyInMinutes: 45,
+    difficulty: getDifficulty(45),
+    aggregateLikes: 16,
+    image: "/img/ic_roasted_carrot.png", // https://img.spoonacular.com/recipes/665203-556x370.jpg
+    iconDiff: "/img/diff.svg",
+    iconPortion: "/img/porcion.svg",
+    iconTime: "/img/timepre.svg",
+  },
+  {
+    id: 4,
+    title: "Crab Salad Stuffed Pita Pockets",
+    servings: 2,
+    readyInMinutes: 25,
+    difficulty: getDifficulty(25),
+    aggregateLikes: 8,
+    image: "/img/ic_cherries.png", // https://img.spoonacular.com/recipes/640318-556x370.jpg
+    iconDiff: "/img/diff.svg",
+    iconPortion: "/img/porcion.svg",
+    iconTime: "/img/timepre.svg",
+  },
+];
+
 function SectionRecipes() {
-  const { data: recipes, isFetching } = useRecipes();
+  // const { data: recipes, isFetching } = useRecipes();
 
   const dataRecipes = useMemo(
     () =>
@@ -28,7 +79,7 @@ function SectionRecipes() {
     [recipes]
   );
 
-  if (isFetching) return <div>Cargando recetas...</div>;
+  // if (isFetching) return <div>Cargando recetas...</div>;
 
   return (
     <section className={style.sectionRecipes}>
