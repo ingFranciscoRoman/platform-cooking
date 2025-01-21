ba46a4c1e88c6c2b76b0f5e2792155636e680c1b
Francisco Roman <franciscoromansierra@gmail.com>
2025-01-20 19:04:53 -0500

    WIP - hydrate

--- components/molecules/RecipeCard/index.tsx
+++ components/molecules/RecipeCard/index.tsx
@@ -1,8 +1,5 @@
-"use client";
-
-import Image from "next/image";
-import style from "@/components/molecules/RecipeCard/RecipeCard.module.scss";
-import { useSplitTitle } from "@/components/molecules/RecipeCard/hooks/useSplitTitle";
+import React, { useState } from "react";
+import style from "@/components/molecules/RecipeCard/HoverCard.module.scss";
 
 interface RecipeCardProps {
   title: string;
@@ -13,92 +10,41 @@ interface RecipeCardProps {
   image: string;
 }
 
-function RecipeCard({
+const HoverCard = ({
   title,
   servings,
   readyInMinutes,
   difficulty,
   aggregateLikes,
   image,
-}: RecipeCardProps) {
-  const { firstTitle, secondTitle } = useSplitTitle(title);
+}: RecipeCardProps) => {
+  const [isHovered, setIsHovered] = useState(false);
 
   return (
-    <div className={style.cardContainer}>
-      <div className={style["container-img"]}>
-        <Image src={image} alt={title} width={160} height={157} />
+    <div
+      className={`${style["hover-card"]} ${isHovered ? style.hover : ""}`}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      {/* Front Card */}
+      <div
+        className={`${style.card} ${style.front} ${
+          isHovered ? style.hidden : ""
+        }`}
+      >
+        Front Card
       </div>
-      <div className={style["container-card-front"]}>
-        <div className={style["card-front"]}>
-          <div className={style["content-title"]}>
-            <p className="font-card-title">{firstTitle}</p>
-            <p className="font-card-title-recipe">{secondTitle}</p>
-          </div>
-          <div className={style["content-information-recipe"]}>
-            <div className={style["information-data-recipe"]}>
-              <Image
-                src="/img/ic_star.svg"
-                alt="icon-portion"
-                width={20}
-                height={20}
-              />
-              <p className="font-card-title">{aggregateLikes}</p>
-            </div>
-            <div>
-              <Image
-                src="/img/ic_heart.svg"
-                alt="icon-portion"
-                width={20}
-                height={20}
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className={style["hidden-card"]}>
-        <div className={style["card-back"]}>
-          <div className={style["content-porcion"]}>
-            <Image
-              src="/img/porcion.svg"
-              width={17}
-              height={17}
-              alt="porcion"
-            />
-            <div>
-              <p className={style["title-item"]}>Tamaño de la porción</p>
-              <p className={style["sub-title-item"]}>{servings} raciones</p>
-            </div>
-          </div>
-          <div className={style["content-time-preparation"]}>
-            <Image
-              src="/img/timepre.svg"
-              width={17}
-              height={17}
-              alt="tiempo de preparación"
-            />
-            <div>
-              <p className={style["title-item"]}>Tiempo de preparación</p>
-              <p className={style["sub-title-item"]}>
-                {readyInMinutes} minutos
-              </p>
-            </div>
-          </div>
-          <div className={style["content-difficulty"]}>
-            <Image
-              src="/img/diff.svg"
-              width={17}
-              height={17}
-              alt="dificultad del plato"
-            />
-            <div>
-              <p className={style["title-item"]}>Dificultad</p>
-              <p className={style["sub-title-item"]}>{difficulty}</p>
-            </div>
-          </div>
-        </div>
+
+      {/* Back Card */}
+      <div
+        className={`${style.card} ${style.back} ${
+          isHovered ? style.visible : ""
+        }`}
+      >
+        Back Card
       </div>
     </div>
   );
-}
+};
 
-export default RecipeCard;
+export default HoverCard;
